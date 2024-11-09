791981034e9fb79907d35e26090cdcf3d65b812a
SunaUezuri <wesley.jane.santos@gmail.com>
2024-11-09 17:40:06 -0300

    Aplicando o conceito de Herança nas classes.

--- oo-sabor-express/models/cardapio/bebida.py
+++ oo-sabor-express/models/cardapio/bebida.py
@@ -1,3 +1,6 @@
+from models.cardapio.item_cardapio import ItemCardapio
+
 class Bebida:
     def __init__(self, nome:str, preco:float, tamanho:str):
-        pass
+        super().__init__(nome, preco)
+        self._tamanho = tamanho
--- oo-sabor-express/models/cardapio/prato.py
+++ oo-sabor-express/models/cardapio/prato.py
@@ -1,3 +1,7 @@
-class Prato:
+from models.cardapio.item_cardapio import ItemCardapio
+
+#Classe com herança aplicada no PYTHON
+class Prato(ItemCardapio):
     def __init__(self, nome:str, preco:float, descricao:str):
-        pass
+        super().__init__(nome, preco)
+        self._descricao = descricao
