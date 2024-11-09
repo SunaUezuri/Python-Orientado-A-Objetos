7cc81b0f1d18873b9c65e6ed455c58bc434925f3
SunaUezuri <wesley.jane.santos@gmail.com>
2024-11-08 22:38:48 -0300

    Substituindo a cadeia de if, elif e elses com o match

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+#Importando bibliotecas
+import os
+
 #Podemos usar triplas aspas para manter uma quebra de linha ao invés do "\n"
-print(''' 
+def exibir_nome_programa():
+    print(''' 
 
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗
@@ -17,23 +21,42 @@ print('''
 ''')
 
 
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurantes')
-print('3. Ativar restaurante')
-print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
+#Definindo uma função
+def finalizar_app():
+    #Limpando o console utiizando a biblioteca os
+    os.system('cls')
+    print('Finalizando programa\n')
 
-print(f'\nVocê escolheu a opção: {opcao_escolhida}')
+def escolher_opcoes():
+    opcao_escolhida = int(input('Escolha uma opção: '))
 
-if opcao_escolhida == '1':
-    print('Cadastro de restaurantes...')
-elif opcao_escolhida == '2':
-    print('Listando restaurantes...')
-elif opcao_escolhida == '3':
-    print('Ativando restaurantes...')
-elif opcao_escolhida == '4':
-    print('Saindo...')
-else:
-    print('Opção inválida!')
+    match opcao_escolhida:
+        case 1:
+            print('Cadastro de restaurantes...')
+        case 2:
+            print('Listando restaurantes...')
+        case 3:
+            print('Ativando restaurantes...')
+        case 4:
+            finalizar_app()
+        case _:
+            print('Opção inválida. Tente novamente.')
+            escolher_opcoes()
+
+
+#Definindo este arquivo como o principal do programa, fazendo com que ele não possa ser importado
+def main():
+    exibir_nome_programa()
+    exibir_opcoes()
+    escolher_opcoes()
+
+
+if __name__ == '__main__':
+    main()
