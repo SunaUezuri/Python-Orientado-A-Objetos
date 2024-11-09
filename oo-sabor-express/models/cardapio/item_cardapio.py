60e4fdbda9dcfa6efa29571c8800b801646bb119
SunaUezuri <wesley.jane.santos@gmail.com>
2024-11-09 18:19:07 -0300

    Iniciando métodos abstratos.

--- oo-sabor-express/models/cardapio/item_cardapio.py
+++ oo-sabor-express/models/cardapio/item_cardapio.py
@@ -1,4 +1,10 @@
-class ItemCardapio:
+from abc import ABC, abstractmethod as abstract
+
+class ItemCardapio(ABC):
     def __init__(self, nome:str, preco:float):
         self._nome = nome
         self._preco = preco
+
+    @abstract
+    def aplicar_desconto(self):
+        pass
