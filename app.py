0bd4e38b0edb25c8c164dbb9ec44fe81b72f869c
SunaUezuri <wesley.jane.santos@gmail.com>
2024-11-08 22:02:19 -0300

    Conhecendo as boas práticas e criando uma variável.

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ print('Sabor Express\n')
 print('1. Cadastrar restaurante')
 print('2. Listar restaurantes')
 print('3. Ativar restaurante')
-print('4. Sair')
+print('4. Sair\n')
 
+opcao_escolhida = input('Escolha uma opção: ')
 
-
+print(f'\nVocê escolheu a opção: {opcao_escolhida}')
