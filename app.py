a8b911dec691bc8bb65586860a6e3039c3556f3e
SunaUezuri <wesley.jane.santos@gmail.com>
2024-11-08 22:21:25 -0300

    Utilizando if, elif's e else.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#Podemos usar triplas aspas para manter uma quebra de linha ao invés do "\n"
 print(''' 
 
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░
@@ -15,7 +16,7 @@ print('''
 ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
 ''')
 
-#Podemos usar triplas aspas para manter uma quebra de linha ao invés do "\n"
+
 
 print('1. Cadastrar restaurante')
 print('2. Listar restaurantes')
@@ -25,3 +26,14 @@ print('4. Sair\n')
 opcao_escolhida = input('Escolha uma opção: ')
 
 print(f'\nVocê escolheu a opção: {opcao_escolhida}')
+
+if opcao_escolhida == '1':
+    print('Cadastro de restaurantes...')
+elif opcao_escolhida == '2':
+    print('Listando restaurantes...')
+elif opcao_escolhida == '3':
+    print('Ativando restaurantes...')
+elif opcao_escolhida == '4':
+    print('Saindo...')
+else:
+    print('Opção inválida!')
