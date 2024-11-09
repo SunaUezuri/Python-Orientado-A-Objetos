cf22bc25accdae35685b396ee520d8c83d0559e6
SunaUezuri <wesley.jane.santos@gmail.com>
2024-11-09 13:00:42 -0300

    Iniciano uma classe python.

---
+++ oo-sabor-express/models/restaurante.py
@@ -0,0 +1,12 @@
+#Criando a classe restaurante
+class Restaurante:
+    nome = ''
+    categoria = ''
+    ativo = False
+
+restaurante_praca = Restaurante()
+restaurante_pizza = Restaurante()
+
+restaurantes = [restaurante_praca, restaurante_pizza]
+
+print(restaurantes)
